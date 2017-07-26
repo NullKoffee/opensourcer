e4f1ceb9f6a90143ad9141fc99699b8c5591ffa5
r3tr0-404 <kaidencook03@gmail.com>
2017-07-26 17:07:02 -0400

    PyTroll
    Troll Game in Python

--- count.py
+++ count.py
@@ -9,11 +9,11 @@ while(timer > 0):
     timer = timer - 1
     user = raw_input("Guess the password or else:")
     if timer == 0:
-        os.system('shutdown /r')
+        os.system('reboot')
 
     elif user == password:
         print "You guessed my password, to bad"
-        os.system('shutdown /r')
+        os.system('reboot')
     
     else:
         continue
